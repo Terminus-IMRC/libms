af86db4434ab27a0f2cb3766d7e4b39040c36072
Terminus <i.can.speak.c.and.basic@gmail.com>
2015-01-10 00:54:57 +0900

    add libms initialization and finalization functions

---
+++ ms.c
@@ -0,0 +1,34 @@
+#include "common.h"
+
+void ms_init(int X, ms_origin_t org, ms_state_t *st)
+{
+	st->X=X;
+	st->Xm2=st->X*2;
+	st->Xm2p2=st->Xm2+2;
+	st->Ceilings=st->X*st->X;
+
+	if(org==MS_ORIGIN_ZERO){
+		/* \sum_{i=0}^{X^2-1}i=\frac{1}{2}X(X^2-1) */
+		st->OneLine=st->X*(st->Ceilings-1)/2;
+	}else if(org==MS_ORIGIN_ONE){
+		/* \sum_{i=1}^{X^2}i=\frac{1}{2}X(X^2+1) */
+		st->OneLine=st->X*(st->Ceilings+1)/2;
+	}
+
+	is_ms_init(st);
+	str_to_ms_init(st);
+	ms_state_print_info_init(st);
+	output_ms_init(st);
+
+	return;
+}
+
+void ms_finalize(ms_state_t *st)
+{
+	is_ms_finalize(st);
+	str_to_ms_finalize(st);
+	ms_state_print_info_finalize(st);
+	output_ms_finalize(st);
+
+	return;
+}
