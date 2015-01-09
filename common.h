d4489e7b6d132133121dd2a5c6f74c2b4405b69b
Terminus <i.can.speak.c.and.basic@gmail.com>
2015-01-10 00:55:23 +0900

    add common header file

---
+++ common.h
@@ -0,0 +1,16 @@
+#ifndef __COMMON_H_INCLUDED__
+#define __COMMON_H_INCLUDED__
+
+#include "libms.h"
+#include "error.h"
+
+	void is_ms_init(ms_state_t *st);
+	void is_ms_finalize(ms_state_t *st);
+	void str_to_ms_init(ms_state_t *st);
+	void str_to_ms_finalize(ms_state_t *st);
+	void ms_state_print_info_init(ms_state_t *st);
+	void ms_state_print_info_finalize(ms_state_t *st);
+	void output_ms_init(ms_state_t *st);
+	void output_ms_finalize(ms_state_t *st);
+
+#endif /* __COMMON_H_INCLUDED__ */
